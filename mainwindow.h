8dba94569d701e672e03d5a172d48afc8966ac94
ChuprinD <chuprin2005@yandex.ru>
2022-03-20 22:31:21 +0300

    added qcustomplot

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include "qcustomplot.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
---
+++ point.h
@@ -0,0 +1,16 @@
+#ifndef POINT_H
+#define POINT_H
+#include <QColor>
+
+
+class Point
+{
+public:
+    /*const int Radius_ = 10;
+    int charge_ = 0;
+    QColor color_ (0, 0 ,0);
+    Point(int charge );*/
+
+};
+
+#endif // POINT_H
